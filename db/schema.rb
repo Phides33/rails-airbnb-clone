8eb32712ef08b9ae14e20787e11cd168ea963ee7
Philippe Despreaux <despreaux.philippe@wanadoo.fr>
2017-11-13 18:48:10 +0100

    model rvs created with associations and validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171113154239) do
+ActiveRecord::Schema.define(version: 20171113163624) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "rvs", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "number_of_guests"
+    t.string   "localisation"
+    t.string   "kind"
+    t.integer  "number_of_beds"
+    t.integer  "price_per_night"
+    t.integer  "min_stay"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
