f7873d812d861c1d25b32eb0eb0b8c550d3b8e31
Reka Kulinyi <reekaa85@gmail.com>
2017-11-13 17:04:33 +0100

    devise setup,added  navbar, added fields to users

---
+++ db/migrate/20171113154239_add_columns_to_users.rb
@@ -0,0 +1,8 @@
+class AddColumnsToUsers < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :date_of_birth, :date
+    add_column :users, :phone_number, :string
+  end
+end
