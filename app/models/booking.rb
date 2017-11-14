e132249da856278c9170d0a77c0b7ff8c9339543
Alexandre Boyer <alexandre.boyer@kedgebs.com>
2017-11-14 10:58:56 +0100

    Booking Model validations and assiociations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,7 +4,7 @@ class Booking < ApplicationRecord
   validates :price, presence: true
   validates :check_in_on, presence: true
   validates :check_out_on, presence: true
-  validates :status, presence: true, inclusion: { in: ["Pending", "Accepted"] }
+  validates :status, presence: true, inclusion: { in: ["Pending", "Accepted", "Cancelled"] }
   validates :rating, inclusion: { in: [0, 1, 2, 3, 4, 5], allow_nil: false }, numericality: { only_integer: true }
   validate :date_validation
   validate :date_is_past
