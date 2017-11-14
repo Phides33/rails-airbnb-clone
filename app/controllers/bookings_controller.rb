28c6598d0421c9546cf64d95e189ddacad23e4dd
Reka Kulinyi <reekaa85@gmail.com>
2017-11-14 18:13:57 +0100

    booking create and update

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -16,6 +16,7 @@ class BookingsController < ApplicationController
     #@rv = Rv.find(params[:rv_id])
     @rv = Rv.find(1)
     @days = (params[:booking][:check_out_on].to_date - params[:booking][:check_in_on].to_date).to_i
+    @booking = Booking.new
     if @days >= @rv.min_stay
       @price = @days * @rv.price_per_night
       @booking = Booking.new(booking_params)
@@ -23,23 +24,36 @@ class BookingsController < ApplicationController
       @booking.rv = @rv
       @booking.user = current_user
       if @booking.save
-
         redirect_to bookings_path
       else
-        raise
+        flash.now[:alert] = @booking.errors.full_messages[0]
         render :new
       end
     else
-      render :new, {alert: "stay must be at least #{@rv.min_stay} long"}
+      flash.now[:alert] = "Stay can't be shorter than #{@rv.min_stay} days"
+      render :new
     end
+  end
 
+  def edit
+    @booking = Booking.find(params[:id])
+  end
 
+  def update
+    @booking = Booking.find(params[:id])
+    @booking.review = params[:booking][:review]
+    @booking.rating = params[:booking][:rating]
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+       render :show
+    end
   end
 
   private
 
   def booking_params
-    params.require(:booking).permit(:check_in_on, :check_out_on)
+    params.require(:booking).permit(:check_in_on, :check_out_on, :rating, :review)
   end
 
 end
